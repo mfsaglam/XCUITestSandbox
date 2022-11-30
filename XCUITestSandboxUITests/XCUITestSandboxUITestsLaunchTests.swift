70228213b6f2c23256a76385ef9b46138ceb7130
mfsaglam <mfsaglam@yandex.com>
2022-11-30 19:24:31 +0300

    add UITestsBundle

---
+++ XCUITestSandboxUITests/XCUITestSandboxUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  XCUITestSandboxUITestsLaunchTests.swift
+//  XCUITestSandboxUITests
+//
+//  Created by Fatih Sağlam on 30.11.2022.
+//  Copyright © 2022 Hacking with Swift. All rights reserved.
+//
+
+import XCTest
+
+final class XCUITestSandboxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
