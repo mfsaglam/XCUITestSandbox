1e25b3417b5f2fce2f1bdd7c09776636ccec42cb
mfsaglam <mfsaglam1@gmail.com>
2022-12-01 20:34:19 +0300

    add first UI tests

--- XCUITestSandboxUITests/XCUITestSandboxUITests.swift
+++ XCUITestSandboxUITests/XCUITestSandboxUITests.swift
@@ -29,6 +29,19 @@ final class XCUITestSandboxUITests: XCTestCase {
         app.launchArguments = ["enable-testing"]
         app.launch()
         
+        app.textFields.element.tap()
+        
+        app.keys["t"].tap()
+        app.keys["e"].tap()
+        app.keys["s"].tap()
+        app.keys["t"].tap()
+        app.keyboards.buttons["Return"].tap()
+        
+        app.sliders["50%"].swipeRight()
+        app.segmentedControls.buttons["Omega"].tap()
+        app.buttons["Blue"].tap()
+        app.alerts["Blue"].buttons["OK"].tap()
+        
         /**
         That will pass “enable-testing” to the app when it’s launched, which we can then make it detect and respond to. Our sandbox app doesn’t have any initial state to worry about, but if you needed to configure your app in a certain way then you’d add something like this to your main app:
          
